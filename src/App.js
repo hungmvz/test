f86550283b75e98b73049749cb4d2dbb01ed57f3
Hung Mai <hungmv@sonix.com.vn>
2023-09-24 13:11:55 +0700

    update code date formatter

--- src/App.js
+++ src/App.js
@@ -1,118 +1,16 @@
-import moment from "moment";
-
-const dateFormat = /^(\d{0,4})(\/)?((\d{1})(\d{1})?)?(\/)?((\d{1})(\d{1})?)?/gm;
-const dateExe = /^(\d{4})(\/)?((\d{1})(\d{1})?)?(\/)?((\d{1})(\d{1})?)?/gm;
+import { useState } from "react";
+import DateInput from "./DateInput";
 
 const App = () => {
-  const onChange = (event) => {
-    const { data, inputType } = event.nativeEvent;
+  const [value, setValue] = useState('');
 
-    console.log('data: ', data, inputType);
-    const value = event.target.value;
-
-    // remove leading zero
-    const removeLeadingZero = value.replace(/^0|^\//gm, '');
-
-    // remove double splash
-    const removeDbSplash = removeLeadingZero.replace(/\/\//gm, '/');
-
-    // remove character that are not number or splash
-    const removeNotDigitOrSplash = removeDbSplash.replace(/[^0-9\/]/, '');
-
-    // prevent input over format
-    const preventInputOver = (removeNotDigitOrSplash.match(dateFormat) || []).join('');
-
-    const [_, year, yearSplash, month, month1, month2, monthSplash, day, day1, day2] = new RegExp(dateFormat).exec(preventInputOver) || [];
-
-    console.log({ _, year, yearSplash, month, month1, month2, monthSplash, day, day1, day2 });
-
-    if (/^insert/gm.test(inputType)) {
-      if (_) {
-        const finalValue = [];
-
-        if (year) {
-          finalValue.push(year);
-          if (year.length === 4) {
-            finalValue.push(yearSplash || '/');
-          }
-
-          if (month) {
-            let firstDay = day1;
-            if (month.length === 1) {
-              if (month1 !== '0') {
-                if (monthSplash) {
-                  finalValue.push(String(month1).padStart(2, '0'));
-                  finalValue.push(monthSplash);
-                } else {
-                  finalValue.push(month1);
-                }
-              } else {
-                finalValue.push(month1);
-                if (monthSplash) {
-                  event.target.value = finalValue.join('');
-                  return;
-                }
-              }
-            }
-            if (month.length === 2) {
-              if (+month > 12) {
-                finalValue.push(String(month1).padStart(2, '0'));
-                finalValue.push('/');
-
-                firstDay = month2;
-              } else {
-                finalValue.push(month);
-                finalValue.push(monthSplash || '/');
-              }
-            }
-
-            if (firstDay) {
-              const date = moment(finalValue.join(''), 'YYYY/MM');
-              const numberOfDay = date.daysInMonth();
-              if (+firstDay * 10 > numberOfDay) {
-                finalValue.push(String(firstDay).padStart(2, '0'));
-                event.target.value = finalValue.join('');
-                return;
-              } else {
-                finalValue.push(firstDay);
-              }
-              if (firstDay === '0') {
-                if (day2 === '0') {
-                  event.target.value = finalValue.join('');
-                  return;
-                }
-              }
-              finalValue.push(day2 || '');
-
-              if (String((firstDay || '') + (day2 || '')).length === 2) {
-                const validDate = moment(finalValue.join(''), 'YYYY/MM/DD', true).format('YYYY/MM/DD');
-                event.target.value = validDate;
-                return;
-              }
-            }
-          }
-        }
-        event.target.value = finalValue.join('');
-      }
-      return;
-    }
-
-    if (!_) {
-      event.target.value = preventInputOver.replace(/[^0-9]/, '');
-    } else {
-      event.target.value = preventInputOver.replace(/\/\//gm, '/');
-    }
-  }
-
-  const onBlur = (event) => {
-    const { value } = event.target;
-    const validDate = moment(value, ['YYYY/MM/D', 'YYYY/MM/DD', 'YYYY/M/D', 'YYYY/M/DD'], true).format('YYYY/MM/DD');
-
-    event.target.value = validDate;
+  const handleDateInputValueChange = (value) => {
+    setValue(value);
   }
 
   return <div>
-    <input onChange={onChange} onBlur={onBlur} placeholder="YYYY/MM/DD" />
+    <DateInput value={value} placeholder='YYYY/MM/DD' onValueChange={handleDateInputValueChange} />
+    Date value: {value}
   </div>
 };
 
---
+++ src/DateInput.js
@@ -0,0 +1,140 @@
+import moment from "moment";
+import { useMemo } from "react";
+
+const dateFormat = /^(\d{0,4})(\/)?((\d{1})(\d{1})?)?(\/)?((\d{1})(\d{1})?)?/gm;
+const dateExe = /^(\d{4})(\/)?((\d{1})(\d{1})?)?(\/)?((\d{1})(\d{1})?)?/gm;
+
+const DateInput = ({ value, placeholder, onValueChange }) => {
+    const defaultValue = useMemo(() => value, [value]);
+
+    const onChange = (event) => {
+        const { data, inputType } = event.nativeEvent;
+
+        const value = event.target.value;
+
+        // remove leading zero
+        const removeLeadingZero = value.replace(/^0|^\//gm, '');
+
+        // remove double splash
+        const removeDbSplash = removeLeadingZero.replace(/\/\//gm, '/');
+
+        // remove character that are not number or splash
+        const removeNotDigitOrSplash = removeDbSplash.replace(/[^0-9\/]/, '');
+
+        // prevent input over format
+        const preventInputOver = (removeNotDigitOrSplash.match(dateFormat) || []).join('');
+
+        const [_, year, yearSplash, month, month1, month2, monthSplash, day, day1, day2] = new RegExp(dateFormat).exec(preventInputOver) || [];
+
+        console.log({ year, yearSplash, month, month1, month2, monthSplash, day, day1, day2 });
+        if (/^insert/gm.test(inputType)) {
+            if (_) {
+                const finalValue = [];
+
+                if (year) {
+                    finalValue.push(year);
+                    if (year.length === 4) {
+                        finalValue.push(yearSplash || '/');
+                    }
+
+                    if (month) {
+                        let firstDay = day1;
+                        let secondDay = day2;
+
+                        const monthLength = month.length;
+
+                        if (monthLength === 1) {
+                            if (month1 === '0' && monthSplash) {
+                                finalValue.push(month1);
+                                event.target.value = finalValue.join('');
+                                onValueChange(finalValue.join(''));
+                                return;
+                            }
+                            if (monthSplash || +month1 > 1) {
+                                finalValue.push(String(month1).padStart(2, '0'));
+                                finalValue.push(monthSplash || '/');
+                            } else {
+                                finalValue.push(month1);
+                            }
+                        }
+
+                        if (monthLength === 2) {
+                            if (month1 === '0' && month2 === '0') {
+                                finalValue.push(month1);
+                                event.target.value = finalValue.join('');
+                                onValueChange(finalValue.join(''));
+                                return;
+                            }
+                            if (+month > 12) {
+                                finalValue.push(String(month1).padStart(2, '0'));
+                                finalValue.push(monthSplash || '/');
+                                firstDay = month2;
+                            } else {
+                                finalValue.push(month);
+                                finalValue.push(monthSplash || '/');
+                            }
+                        }
+
+                        if (firstDay) {
+                            const date = moment(finalValue.join(''), 'YYYY/MM');
+                            const numberOfDay = date.daysInMonth();
+
+                            if (firstDay === '0' && day2 === '0') {
+                                event.target.value = finalValue.join('');
+                                onValueChange(finalValue.join(''));
+                                return;
+                            }
+
+                            if (+firstDay * 10 > numberOfDay) {
+                                finalValue.push(String(firstDay).padStart(2, '0'));
+                                event.target.value = finalValue.join('');
+
+                                firstDay = '0';
+                                secondDay = firstDay;
+                            } else {
+                                finalValue.push(firstDay);
+                            }
+
+                            finalValue.push(day2 || '');
+
+                            if (String(firstDay + secondDay).length === 2) {
+                                const validDate = moment(finalValue.join(''), ['YYYY/M/DD'], true).format('YYYY/MM/DD');
+
+                                event.target.value = validDate;
+
+                                onValueChange(validDate);
+
+                                return;
+                            }
+                        }
+                    }
+                }
+                onValueChange(finalValue.join(''));
+                event.target.value = finalValue.join('');
+            }
+            return;
+        }
+
+        if (!_) {
+            event.target.value = preventInputOver.replace(/[^0-9]/, '');
+        } else {
+            event.target.value = preventInputOver.replace(/\/\//gm, '/');
+        }
+        onValueChange(event.target.value);
+    }
+
+    const onBlur = (event) => {
+        const { value } = event.target;
+        const validDate = moment(value, ['YYYY/MM/D', 'YYYY/MM/DD', 'YYYY/M/D', 'YYYY/M/DD'], true).format('YYYY/MM/DD');
+
+        event.target.value = validDate;
+
+        onValueChange(validDate);
+    }
+
+    return <div>
+        <input defaultValue={defaultValue} onBlur={onBlur} onChange={onChange} placeholder={placeholder} />
+    </div>
+}
+
+export default DateInput;
