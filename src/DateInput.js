4483b954d1b0cb79a4109dcb7c5e9f3227666a3c
Hung Mai <hungmv@sonix.com.vn>
2023-09-24 21:44:31 +0700

    update

--- src/DateInput.js
+++ src/DateInput.js
@@ -6,6 +6,76 @@ const dateFormat = /^(\d{0,4})(\/)?(\d{0,2})(\/)?(\d{0,2})/gm;
 const DateInput = ({ value, placeholder, onValueChange }) => {
     const defaultValue = useMemo(() => value, [value]);
 
+    const handleChangeYear = (input, length, separate) => {
+        const result = [input];
+
+        if (input.length === length) {
+            result.push(separate || '/');
+        }
+
+        return result.join('');
+    };
+
+    const handleChangeMonth = (input, separate) => {
+        if (!input) {
+            return {};
+        }
+        // remove 00 or 0/ by 0 (prevent input double zero or splash after zero)
+        const m = [input].join('').replace(/^00|^0\//gm, '0');
+
+        const m1 = m[0];
+        const m2 = m[1];
+
+        const monthLength = m.length;
+
+        if (monthLength === 1) {
+            if (+m1 > 1) {
+                return {
+                    month: String(m1).padStart(2, '0') + (separate || '/'),
+                }
+            } else {
+                return {
+                    month: m1,
+                }
+            }
+        }
+
+        if (monthLength === 2) {
+            if (+m > 12) {
+                return {
+                    month: String(m1).padStart(2, '0') + (separate || '/'),
+                    remain: m2,
+                }
+            } else {
+                return {
+                    month: m + (separate || '/'),
+                }
+            }
+        }
+    }
+
+    const handleChangeDay = (input = '', value, remainMonth) => {
+        const date = moment(value, 'YYYY/MM');
+        const numberOfDay = date.daysInMonth();
+
+        // remove double zero
+        const d = [input].join('').replace(/^00/gm, '0');
+
+        let firstDay = remainMonth || d[0];
+        let secondDay = d[1];
+
+        if (+firstDay * 10 > numberOfDay) {
+            secondDay = firstDay;
+            firstDay = '0';
+        }
+
+        const result = [];
+        result.push(firstDay || '');
+        result.push(secondDay || '');
+
+        return result.join('');
+    }
+
     const onChange = (event) => {
         const { inputType } = event.nativeEvent;
 
@@ -23,83 +93,35 @@ const DateInput = ({ value, placeholder, onValueChange }) => {
         // prevent input over format
         const preventInputOver = (removeNotDigitOrSplash.match(dateFormat) || []).join('');
 
-        const [_, year, yearSplash, month, monthSplash, day, day1, day2] = new RegExp(dateFormat).exec(preventInputOver) || [];
+        const [_, yearInput, yearSplash, monthInput, monthSplash, dayInput] = new RegExp(dateFormat).exec(preventInputOver) || [];
 
-        // console.log({ year, yearSplash, month, monthSplash, day1, day2 })
         if (/^insert/gm.test(inputType)) {
             if (_) {
-                const finalValue = [];
+                const values = [];
 
-                if (year) {
-                    finalValue.push(year);
-                    if (year.length === 4) {
-                        finalValue.push(yearSplash || '/');
-                    }
+                // push year value
+                values.push(handleChangeYear(yearInput, 4, yearSplash));
+
+                // push month value and calculate remain month (if available)
+                const { month, remain } = handleChangeMonth(monthInput, monthSplash);
+                values.push(month);
+
+                // push day value
+                values.push(handleChangeDay(dayInput, values.join(''), remain));
+
+                const dateValue = values.join('');
+                if (dateValue.length === 'YYYY/MM/DD'.length) {
+                    const validDate = moment(dateValue, ['YYYY/MM/DD'], true).format('YYYY/MM/DD');
+
+                    event.target.value = validDate;
+
+                    onValueChange(validDate);
+
+                    return;
                 }
 
-                let remainMonth = '';
-
-                if (month) {
-                    // remove 00 or 0/ by 0 (prevent input double zero or splash after zero)
-                    const m = [month].join('').replace(/^00|^0\//gm, '0');
-
-                    const m1 = m[0];
-                    const m2 = m[1];
-
-                    const monthLength = m.length;
-
-                    if (monthLength === 1) {
-                        if (+m1 > 1) {
-                            finalValue.push(String(m1).padStart(2, '0'));
-                            finalValue.push(monthSplash || '/');
-                        } else {
-                            finalValue.push(m1);
-                        }
-                    }
-
-                    if (monthLength === 2) {
-                        if (+m > 12) {
-                            finalValue.push(String(m1).padStart(2, '0'));
-                            finalValue.push(monthSplash || '/');
-                            remainMonth = m2;
-                        } else {
-                            finalValue.push(m);
-                            finalValue.push(monthSplash || '/');
-                        }
-                    }
-                }
-
-                if (remainMonth || day) {
-                    const date = moment(finalValue.join(''), 'YYYY/MM');
-                    const numberOfDay = date.daysInMonth();
-
-                    // remove double zero
-                    const d = [day].join('').replace(/^00/gm, '0');
-
-                    let firstDay = remainMonth || d[0];
-                    let secondDay = d[1];
-
-                    if (+firstDay * 10 > numberOfDay) {
-                        secondDay = firstDay;
-                        firstDay = '0';
-                    }
-
-                    finalValue.push(firstDay || '');
-                    finalValue.push(secondDay || '');
-
-                    if (d.length === 2) {
-                        const validDate = moment(finalValue.join(''), ['YYYY/MM/DD'], true).format('YYYY/MM/DD');
-
-                        event.target.value = validDate;
-
-                        onValueChange(validDate);
-
-                        return;
-                    }
-                }
-
-                event.target.value = finalValue.join('');
-                onValueChange(finalValue.join(''));
+                event.target.value = dateValue;
+                onValueChange(dateValue);
             }
             return;
         }
